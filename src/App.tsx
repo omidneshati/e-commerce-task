5f62828c3fbfd49466e9da76cecdf8cb5900d57c
Omid-Neshati <tchandnofame@gmail.com>
2023-10-22 19:13:01 +0330

    add product & selectedCategories & sidebar stage add provider and set values add sidebar add navbar and Home (product list) page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import ProductType from "./interfaces/Product/Product";
+import Context from "./context/Context";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Context.Provider
+      value={{
+        selectedCategories,
+        setSelectedCategories,
+        products,
+        setProducts,
+        sidebarOpen,
+        setSidebarOpen,
+      }}>
+      <div className="">
+        <Navbar />
+        <div className="d-flex">
+          <aside
+            className={`${
+              sidebarOpen
+                ? "d-block position-fixed w-100 z-2 bg-white"
+                : "d-none"
+            } d-sm-block min-vh-100 w-25 p-3`}>
+            <Sidebar />
+          </aside>
+          <main className="container  d-flex justify-content-center ">
+            <Home />
+          </main>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Context.Provider>
+  );
 }
 
-export default App
+export default App;
